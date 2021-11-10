766bcd3ba08d6484fa500593e88c0ed4adf53fd7
daniel-nguyen <daniel.n.q.nguyen@gmail.com>
2021-11-10 14:13:30 +1100

    Define reserved_seats in Flights model and allow json extracting

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -2,4 +2,10 @@ class Flight < ApplicationRecord
   belongs_to :airplane
   has_many :reservations
   has_and_belongs_to_many :users
+
+  def booked_seats
+    reservations.map do |r|
+      { :row => r.row, :column => r.column }
+    end
+  end
 end
