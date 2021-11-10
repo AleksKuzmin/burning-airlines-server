c79aaa4c1cf246a2a80de369b13929c22a5502eb
Angeline Polidano <angelinepolidano@gmail.com>
2021-11-10 14:25:07 +1100

    fixed for merge

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -2,10 +2,4 @@ class Flight < ApplicationRecord
   belongs_to :airplane
   has_many :reservations
   has_and_belongs_to_many :users
-
-  def booked_seats
-    reservations.map do |r|
-      { :row => r.row, :column => r.column }
-    end
-  end
 end
