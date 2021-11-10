8a841e424c0eedd606020db61b8c3c914e6a4246
daniel-nguyen <daniel.n.q.nguyen@gmail.com>
2021-11-10 14:23:56 +1100

    Rename booked_seats to reserved_seats

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -3,7 +3,9 @@ class Flight < ApplicationRecord
   has_many :reservations
   has_and_belongs_to_many :users
 
-  def booked_seats
+  # TODO: get all seats
+
+  def reserved_seats
     reservations.map do |r|
       { :row => r.row, :column => r.column }
     end
