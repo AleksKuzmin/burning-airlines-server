8b898222e7dcf025f05edcbf66b2ea99a7b7c976
daniel-nguyen <daniel.n.q.nguyen@gmail.com>
2021-11-11 12:41:37 +1100

    Generate Session controller, routes for login/logout

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,8 @@ Rails.application.routes.draw do
   resources :users
   resources :flights
   resources :airplanes
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get '/login' => 'session#new'
+  post '/login' => 'session#create'
+  delete '/login' => 'session#destroy'
 end
